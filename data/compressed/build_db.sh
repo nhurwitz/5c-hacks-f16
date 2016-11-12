014945db6d24e56404052d162e195fe0245fe3bc
Dmitriy Smirnov <dimkasmir@gmail.com>
2016-11-12 01:09:13 -0800

    fix csv to postgres

--- data/compressed/build_db.sh
+++ data/compressed/build_db.sh
@@ -1,19 +1,70 @@
-gunzip -ck GeoLite2-City-Blocks-IPv4.csv.gz > ../IP2Zip.csv
-gunzip -ck hardcoded-dave-data.csv.gz > ../hardcoded_dave_data.csv
-gunzip -ck zip2unemployment.csv.gz > ../zip2unemployment.csv
-gunzip -ck zip2popdensity-area.csv.gz > ../zip2popdensity_area.csv
+gunzip -ck GeoLite2-City-Blocks-IPv4.csv.gz > IP2Zip.csv
+gunzip -ck hardcoded-dave-data.csv.gz > hardcoded_dave_data.csv
+gunzip -ck zip2unemployment.csv.gz > zip2unemployment.csv
+gunzip -ck zip2popdensity-area.csv.gz > zip2popdensity_area.csv
 echo "
-CREATE TABLE ip2Zip (ip reap ,geoname_id int,registered_country_geoname_id int,represented_country_geoname_id int,is_anonymous_proxy int,is_satellite_provider int,zip real,latitude real,longitude real,accuracy_radius int);
-.mode csv
-.import ../IP2Zip.csv ip2Zip
-CREATE TABLE dave_data (ip real,DictatorGame real,PrisonersDilemma real,CharitableGiving real,Honesty real,PunishmentOfSelfish real,PunishmentOfFairness real,Impatience_time_discountin real,reflectiveness_correct_answers_on_CRT real,big5_extraversion real,big5_agreeableness real,big5_conscientiousness real,big5_emotionalstability real,big5openness real,age real,female real,graduated_from_college,income_over_35k real,passed_comprehension_questions real,risk_appetite real,trust real,Support_for_Rep1_vs_Dem7,god real,log10_number_of_studies_completed real);
-.import ../hardcoded_dave_data.csv dave_data
-CREATE TABLE unemployment (zip real,unemp_rate text,num_in_sample real);
-.import ../zip2unemployment.csv unemployment
-CREATE TABLE popdense (zip real,population real,land_sq_mi real,density_per_sq_mile real);
-.import ../zip2popdensity_area.csv popdense
-	" | sqlite3 data.db
-rm ../IP2Zip.csv
-rm ../hardcoded_dave_data.csv
-rm ../zip2unemployment.csv
-rm ../zip2popdensity_area.csv
+DROP DATABASE ip;
+CREATE DATABASE ip;
+\c ip
+
+CREATE TABLE ip_to_zip (
+    ip inet,
+    geoname_id int,
+    registered_country_geoname_id int,
+    represented_country_geoname_id int,
+    is_anonymous_proxy boolean,
+    is_satellite_provider boolean,
+    zip text,
+    latitude real,
+    longitude real,
+    accuracy_radius int
+);
+COPY ip_to_zip FROM '$PWD/IP2Zip.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE dave_data (
+    ip inet,
+    dictator_game real,
+    prisoners_dilemma real,
+    charitable_giving real,
+    honesty real,
+    punishment_of_selfish real,
+    punishment_of_fairness real,
+    impatience_time_discountin real,
+    reflectiveness_correct_answers_on_crt real,
+    big5_extraversion real,
+    big5_agreeableness real,
+    big5_conscientiousness real,
+    big5_emotionalstability real,
+    big5openness real,
+    age int,
+    female boolean,
+    graduated_from_college boolean,
+    income_over_35k boolean,
+    passed_comprehension_questions boolean,
+    risk_appetite real,
+    trust real,
+    support_for_rep1_vs_dem7 real,
+    god real,
+    log10_number_of_studies_completed real
+);
+COPY dave_data FROM '$PWD/hardcoded_dave_data.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE unemployment (
+    zip text,
+    unemp_rate text,
+    num_in_sample real
+);
+COPY unemployment FROM '$PWD/zip2unemployment.csv' DELIMITER ',' CSV HEADER;
+
+CREATE TABLE popdense (
+    zip text,
+    population int,
+    land_sq_mi real,
+    density_per_sq_mile real
+);
+COPY popdense FROM '$PWD/zip2popdensity_area.csv' DELIMITER ',' CSV HEADER;
+	" | psql
+rm IP2Zip.csv
+rm hardcoded_dave_data.csv
+rm zip2unemployment.csv
+rm zip2popdensity_area.csv
