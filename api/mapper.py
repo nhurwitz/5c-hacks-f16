50103918a003e5f9c4908eaed8a36051ff1dce2c
Ted Kornish <ted.kornish+github@gmail.com>
2016-11-12 01:58:59 -0800

    Add logic for getting ip and census data to mapper

--- api/mapper.py
+++ api/mapper.py
@@ -1,11 +1,30 @@
-import sqlite3
+import psycopg2
 
 class Mapper:
     def __init__(self):
-        self._conn = sqlite3.connect('../data/compressed/data.db')
+        self._conn = psycopg2.connect("dbname='ip'")
 
-    def ip_to_zip(ip):
-        pass
+    def ip_to_zip(self, ip):
+        """ Returns a zip code as a string or false if there is no zip
+        code. We make no assertions about the format of `ip`.
+        """
+        cur = self._conn.cursor()
+        cur.execute("SELECT zip FROM ip_to_zip WHERE ip >> inet '{}'".format(ip))
+        results = cur.fetchall()
+        if len(results) > 0:
+            return results[0][0]
+        else:
+            return false
 
-    def zip_to_census_data(zip):
-        pass
+    def zip_to_census_data(self, zip):
+        cur = self._conn.cursor()
+        cur.execute("""
+        SELECT u.*, p.*
+        FROM unemployment u
+        JOIN popdense p ON u.zip = p.zip
+        WHERE u.zip = '{}'
+        """.format(zip))
+        results = cur.fetchall()
+        return results[0]
+
+m = Mapper()
