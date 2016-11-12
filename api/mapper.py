9e307447f01693929f09cc914dc25a5849b77c11
Ted Kornish <ted.kornish+github@gmail.com>
2016-11-11 23:33:04 -0800

    Add Mapper class

---
+++ api/mapper.py
@@ -0,0 +1,11 @@
+import sqlite3
+
+class Mapper:
+    def __init__(self):
+        self._conn = sqlite3.connect('../data/compressed/data.db')
+
+    def ip_to_zip(ip):
+        pass
+
+    def zip_to_census_data(zip):
+        pass
